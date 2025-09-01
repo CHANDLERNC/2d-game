7ea9d67f476c8c4f696b217b0c4a2608f004e80b
CHANDLERNC <80640595+CHANDLERNC@users.noreply.github.com>
2025-09-01 09:35:13 -0500

    test: cover new inventory slots

--- test/save-load.test.js
+++ test/save-load.test.js
@@ -2,13 +2,13 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 
 import { player } from '../modules/player.js';
-import { inventory } from '../modules/playerInventory.js';
+import { inventory, SLOTS } from '../modules/playerInventory.js';
 import { getSaveData, applySaveData } from '../modules/saveLoad.js';
 
 function setupState(){
   // ensure known clean state? after import player & inventory already set.
   // We'll reset arrays/equip to ensure test isolation
-  inventory.equip = { helmet:null, chest:null, legs:null, hands:null, feet:null, weapon:null };
+  inventory.equip = Object.fromEntries(SLOTS.map(s => [s, null]));
   inventory.bag = new Array(inventory.bag.length).fill(null);
   inventory.potionBag = new Array(inventory.potionBag.length).fill(null);
   player.skillPoints = 0;
@@ -27,6 +27,7 @@ test('inventory and abilities persist through save data', () => {
   setupState();
   // give player some state
   inventory.equip.weapon = { name: 'Sword of Tests' };
+  inventory.equip.necklace = { name: 'Amulet of Tests' };
   inventory.bag[0] = { name: 'Health Potion' };
   inventory.potionBag[0] = { name: 'Mana Potion' };
   player.skillPoints = 2;
@@ -46,6 +47,7 @@ test('inventory and abilities persist through save data', () => {
   assert.equal(inventory.equip.weapon.name, 'Sword of Tests');
   assert.equal(inventory.bag[0].name, 'Health Potion');
   assert.equal(inventory.potionBag[0].name, 'Mana Potion');
+  assert.equal(inventory.equip.necklace.name, 'Amulet of Tests');
   assert.equal(player.skillPoints, 2);
   assert.equal(player.magicPoints, 3);
   assert.ok(player.skills.berserkerBattle[0]);
